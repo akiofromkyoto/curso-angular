1492735c6f2a41feab232b87907bfb479e39eb98
e_erikm <e_erikm@sebraesp.com.br>
2023-08-14 15:41:45 -0300

    atualizado

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Animal } from 'src/app/Animal';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent {
+  animals : Animal[] = [
+    {name: "Turca", type: "Dog", age: 4},
+    {name: "Tom", type: "Cat", age: 40},
+    {name: "frida", type: "Dog", age: 14},
+    {name: 'Bob', type:"Horse", age: 24},
+  ];
+
+}
